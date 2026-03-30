4d942476a4b0fa8bc0554de25f95238c1d2505cb
Pankaj <pankajpriscilla@gmail.com>
2026-03-30 02:45:23 -0400

    Bump version to 3.1.10; remove extraneous comma in `sendResponse.js` function args.
    Signed-off-by: Pankaj <pankajpriscilla@gmail.com>

--- src/utils/sendResponse.js
+++ src/utils/sendResponse.js
@@ -29,7 +29,7 @@ const sendResponse = ({
   data = null,
   error = null,
   pagination = null,
-  meta = {},
+  meta = {}
 }) => {
   const standardMeta = {
     version: process.env.API_VERSION || '1.0.0',
