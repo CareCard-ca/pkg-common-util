362bfe3bd5f1e8547094f9462c1b2c122b6df47d
Pankaj Singh <pankajpriscilla@gmail.com>
2026-08-05 21:26:21 -0400

    Tests Restored
    Signed-off-by: Pankaj Singh <pankajpriscilla@gmail.com>

---
+++ test/index.test.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const { runIndexedMochaTests } = require('../scripts/testParallel/runIndexedMochaTests.cjs');
+
+const parallelTestFiles = [
+  'test/appErrorHandlers.test.js',
+  'test/applicationLogging.test.js',
+  'test/applicationLoggingDocumentation.test.js',
+  'test/config/repositoryIsolation.test.js',
+  'test/config/tddGuidanceDocs.test.js',
+  'test/errorUtils.test.js',
+  'test/keysCaseConverter.test.js',
+  'test/requestContext_fallback.test.js',
+  'test/responseStatus.test.js',
+  'test/srcIndex.test.js',
+  'test/standardResponse.test.js',
+  'test/traceContext.test.js',
+  'test/traceDocumentation.test.js',
+  'test/traceEsmExports.test.mjs',
+  'test/loggingEsmExports.test.mjs',
+  'test/openTelemetryBridge.test.js',
+  'test/utilityFunctions.test.js'
+];
+
+if (require.main === module) {
+  runIndexedMochaTests(parallelTestFiles)
+    .then((exitCode) => {
+      process.exitCode = exitCode;
+    })
+    .catch((error) => {
+      console.error(error);
+      process.exitCode = 1;
+    });
+}
+
+module.exports = { parallelTestFiles };
